0827d72be5c9dcec5e8e3ce0b6f8905c750f4c5b
Sanghak Han <gkstkdgkr12@gmail.com>
2022-01-17 04:28:45 +0900

    commit

--- indeed.py
+++ indeed.py
@@ -17,12 +17,36 @@ def extract_indeed_pages():
 
   max_page = pages[-1]
   return max_page
-
-
+  
 
 def extract_indeed_jobs(last_page):
   jobs = []
   for page in range(last_page):
+    print(f"Scrapping page {page}")
     result =requests.get(f"{url}&start={page*limit}")
-    print(result.status_code)
+    soup = bs(result.text, "html.parser")
+    results = soup.find_all("a",{"class":"result"})
+    for result in results:
+      title = result.find("h2",{"class":"jobTitle"})
+      title_span = title.find("span")
+      if title_span.has_attr("title") is True:
+        title = str(title_span.string)
+      else:
+        continue
+      company = result.find("span",{"class":"companyName"})
+      if company is not None:
+        company = str(company.string)
+      else:
+        company = str(company.find("a",{"class":"companyOverviewLink"}).string)
+      location = result.find("div",{"class":"companyLocation"}).text
+      job_id = result["data-jk"]
+      job = {
+        'title':title, 
+        'company':company, 
+        'location':location, 
+        'link':f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50&vjk={job_id}"
+      }
+      jobs.append(job)
+
+      
   return jobs
